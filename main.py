a002deab8ab1f7088b018af70dfa3b2f2b4c5d7b
sammy-jay <sammyjayden27@gmail.com>
2022-07-20 04:28:59 +0100

    learnt about query params

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
+from typing import Union
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
-# Order matters
-@app.get('/items/me')
-def read_user_me():
-    return {"user_id": "22"}
+fake_items_db = [{"item_name": "Foo"}, {
+    "item_name": "Bar"}, {"item_name": "Baz"}]
+
 
 @app.get('/items/{item_id}')
-async def root(item_id:int):
-    return {"message": f"Item ID: {item_id}"}
+def read_item(item_id: str, q: Union[str, None] = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item['q'] = q
+    if not short:
+        item['description'] = "This is an amazing item"
+
+    return item
+
+
+@app.get('/users/{user_id}/items/{item_id}')
+def read_user_item(user_id: str, item_id: str, category: str = "all", order: Union[str, None] = None):
+    return {
+        "user_id": user_id,
+        "item_id": item_id,
+        "category": category,
+        "order": order
+    }
