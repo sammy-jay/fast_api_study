63b88fae57d333d611e956b678a8e581c3b2feea
sammy-jay <sammyjayden27@gmail.com>
2022-07-20 04:04:08 +0100

    learnt about path parameters and how order matters

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
-async def root():
-    return {"message": "my first python api"}
+
+# Order matters
+@app.get('/items/me')
+def read_user_me():
+    return {"user_id": "22"}
+
+@app.get('/items/{item_id}')
+async def root(item_id:int):
+    return {"message": f"Item ID: {item_id}"}
