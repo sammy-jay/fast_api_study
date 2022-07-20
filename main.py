040df605fdb215dcf41910f76242afe3e1906f52
sammy-jay <sammyjayden27@gmail.com>
2022-07-20 04:42:43 +0100

    learnt how to accept json body and modelling with pydantic

--- main.py
+++ main.py
@@ -1,29 +1,23 @@
 from typing import Union
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
 
 app = FastAPI()
 
 
-fake_items_db = [{"item_name": "Foo"}, {
-    "item_name": "Bar"}, {"item_name": "Baz"}]
-
-
-@app.get('/items/{item_id}')
-def read_item(item_id: str, q: Union[str, None] = None, short: bool = False):
-    item = {"item_id": item_id}
-    if q:
-        item['q'] = q
-    if not short:
-        item['description'] = "This is an amazing item"
-
-    return item
-
-
-@app.get('/users/{user_id}/items/{item_id}')
-def read_user_item(user_id: str, item_id: str, category: str = "all", order: Union[str, None] = None):
+@app.post('/items/{item_id}')
+def create_item(item: Item, item_id: str):
+    item_dict = item.dict()
+    print(item_dict)
     return {
-        "user_id": user_id,
-        "item_id": item_id,
-        "category": category,
-        "order": order
+        "item": item,
+        "item_id": item_id
     }
